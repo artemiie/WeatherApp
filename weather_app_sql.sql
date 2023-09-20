f507e4fc2bd47678f233267cf8350a2641753d71
Artemie Fliostor <artiom.fliostor@gmail.com>
2023-09-20 19:42:08 +0300

    Add files

---
+++ weather_app_sql.sql
@@ -0,0 +1,10 @@
+create database weather_app_db;
+use weather_app_db;
+
+drop table WA_USER;
+drop table USER_SESSION;
+drop table USER_LOCATION;
+
+select * from wa_user;
+select * from user_session;
+select * from user_location;
